bb2e27a58349a8ae7f98f8ab602aeef1d363e0e2
29534411212 <tkalyoncuoglu@gmail.com>
2023-08-20 23:19:19 +0300

    cleaned up some code

--- src/redux/jobslice.js
+++ src/redux/jobslice.js
@@ -18,30 +18,6 @@ const jobSlice = createSlice({
     addJob: (state, action) => {
       state.jobs.push(action.payload);
     },
-    // Arama terimine göre filtreleme
-    filterBySearch: (state, action) => {
-      // arama terimini küçük harfe çevirme(duyarlılığı ortadan kalfırmak için)
-      const query = action.payload.toLowerCase();
-      //aksiyon ile gelen arama terimiyle eşleşen objelerle yeni bir dizi oluştur
-      const filtered = state.jobs.filter((job) =>
-        job.company.toLowerCase().includes(query)
-      );
-
-      // store güncelleme
-      state.filteredJobs = filtered;
-    },
-    // Duruma Göre Filtreleme
-    filterByStatus: (state, action) => {
-      // Aksiyonun payload değeriyle eşleşen işlerle yeni bir dizi oluştur
-      state.filteredJobs = state.jobs.filter(
-        (job) => job.status == action.payload
-      );
-    },
-    filterByType: (state, action) => {
-      state.filteredJobs = state.jobs.filter(
-        (job) => job.type == action.payload
-      );
-    },
     filter: (state, action) => {
       const {search, status, type, sort} = action.payload;
 
@@ -111,45 +87,6 @@ const jobSlice = createSlice({
 
       state.filteredJobs = filteredJobs;
 
-    },
-    filterBySort: (state, action) => {
-      switch (action.payload) {
-        case "a-z":
-          state.filteredJobs.sort((a, b) => {
-            // eğerki a objesinin şirket ismi alfabede sıra olarak
-            // b'den gerideyse a objesini b'ye göre daha ön sıraya koy
-            // ! sort dizideki bütün elemanlar için bu sorguyu gerçekleştitir
-            if (a.company < b.company) return -1;
-            if (a.company > b.company) return 1;
-            return 0;
-          });
-          break;
-        case "z-a":
-          state.filteredJobs.sort((a, b) => {
-            // eğerki a objesinin şirket ismi alfabede sıra olarak
-            // b'den gerideyse a objesini b'ye göre daha ön sıraya koy
-            // ! sort dizideki bütün elemanlar için bu sorguyu gerçekleştitir
-            if (a.company < b.company) return 1;
-            if (a.company > b.company) return -1;
-            return 0;
-          });
-
-        case "En Yeni":
-          state.filteredJobs.sort(
-            (a, b) => new Date(b.date) - new Date(a.date)
-          );
-          break;
-
-        case "En Eski":
-          state.filteredJobs.sort(
-            (a, b) => new Date(a.date) - new Date(b.date)
-          );
-
-        default:
-          break;
-      }
-
-      return state;
     },
     // filtreli temizle
     clearFilters: (state) => {
